ba6787328964e40c1d4e3a501ef1d8126adbaf78
nickcedwards <nickedwrds@gmail.com>
2024-01-04 12:54:12 +1300

    Reactor webapp to add config and status to the main route

--- webapp.py
+++ webapp.py
@@ -1,38 +1,56 @@
 import sqlite3
 import config
 from flask import Flask, jsonify
+from functools import wraps
 import jinja2
 
 templateLoader = jinja2.FileSystemLoader( searchpath="." )
-templateEnv = jinja2.Environment( loader=templateLoader )
+template_env = jinja2.Environment( loader=templateLoader )
 
 app = Flask(__name__)
 
-def get_latest():
+def get_cursor():
     con = sqlite3.connect(config.DB)
     con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT * FROM readings ORDER BY timestamp DESC LIMIT 1")
-    result = cur.fetchone()
-    templateVars = { k:result[k] for k in result.keys() }
-    template = templateEnv.get_template( 'home.jinja' )
-    return template.render( templateVars )
+    return con.cursor()
 
+def return_json(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        response = jsonify(f())
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    return wrap
+
+def add_single_row(cur, query, template_vars):
+    cur.execute(query)
+    row = cur.fetchone()
+    if not row:
+        return
+    for k in row.keys():
+        if k != 'id':
+            template_vars[k] = row[k]
 
 @app.route('/')
 def main():
-    return get_latest()
+    cur = get_cursor()
+    template_vars = {
+        'vbat': -1,
+        'temperature': -1,
+        'humidity': -1,
+    }
+    add_single_row(cur, "SELECT * FROM readings ORDER BY timestamp DESC LIMIT 1", template_vars)
+    add_single_row(cur, "SELECT * FROM status LIMIT 1", template_vars)
+    add_single_row(cur, "SELECT * FROM settings ORDER BY id DESC LIMIT 1", template_vars)
+    template = template_env.get_template( 'home.jinja' )
+    return template.render( template_vars )
 
 @app.route('/data')
+@return_json
 def data():
-    con = sqlite3.connect(config.DB)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
+    cur = get_cursor()
     cur.execute("SELECT * FROM readings WHERE timestamp > datetime('now' , '-7 days');")
-    result = [{k:row[k] for k in row.keys()} for row in cur.fetchall()]
-    response = jsonify(result)
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return [{k:row[k] for k in row.keys()} for row in cur.fetchall()]
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
